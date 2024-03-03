e59e2e53b295618264d519baa7891a0729d9d62a
Jan Nogueira <jannogueiraacosta@gmail.com>
2024-03-03 23:30:25 +0100

    entrega 1

---
+++ juego.c
@@ -0,0 +1,68 @@
+#include <my_global.h>
+#include <mysql.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	MYSQL *conn;
+	int err;
+	MYSQL_RES *resultado;
+	MYSQL_ROW row;
+	
+	//Creamos una conexion al servidor MYSQL 
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexi￳n: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//inicializar la conexion
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "JUEGO",0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	
+	char saberpuntuacion[20];
+	// Pregunto los nombre de los dos jugadores
+	printf ("¿Quieres saber cual es nombre del jugador con la mayor puntuacion?\nS o s para sí, N o n para no:\n"); 
+	scanf ("%s", &saberpuntuacion);
+	
+	char consulta [80];
+	if(saberpuntuacion == 'S' or saberpuntuacion == 's')
+		strcpy (consulta,"SELECT * FROM JUGADORES WHERE PUNTUACION = (SELECT MAX(PUNTUACION) FROM JUGADORES)");
+	
+	err=mysql_query (conn, consulta);
+	if (err!=0) {
+		printf ("Error al consultar datos de la base %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//recogemos el resultado de la consulta. El resultado de la
+	//consulta se devuelve en una variable del tipo puntero a
+	//MYSQL_RES tal y como hemos declarado anteriormente.
+	//Se trata de una tabla virtual en memoria que es la copia
+	//de la tabla real en disco.
+	resultado = mysql_store_result (conn);
+	// El resultado es una estructura matricial en memoria
+	// en la que cada fila contiene los datos de una persona.
+	
+	// Ahora obtenemos la primera fila que se almacena en una
+	// variable de tipo MYSQL_ROW
+	row = mysql_fetch_row (resultado);
+	
+	if (row == NULL)
+		printf ("No se han obtenido datos en la consulta\n");
+	else
+		while (row !=NULL) {
+			// la columna 0 contiene el nombre del jugador
+			printf ("%s\n", row[0]);
+			// obtenemos la siguiente fila
+			row = mysql_fetch_row (resultado);
+	}
+	
+	mysql_close (conn);
+	exit(0);
+}
